918794f1cc86b0cd0eb11a5bb7e6934c1f5c2ac5
yashk <devyk100@gmail.com>
2024-06-01 17:25:31 +0530

    accept connections

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include "http_server.h"
 int main(){
+    using namespace http;
     std::cout << "Hello world" << std::endl;
+    TcpServer server("0.0.0.0", 8080);
+    server.startListen();
 }
--- src/server/http_server.cpp
+++ src/server/http_server.cpp
@@ -1,6 +1,13 @@
 #include "http_server.h"
 #include <sstream>
 #include <unistd.h>
+#include <iostream>
+
+namespace {
+    const int BUFFER_SIZE = 30720;
+
+}
+
 namespace http{
     TcpServer::TcpServer(std::string ip_address, int port): 
     serverSocket(), 
@@ -12,6 +19,9 @@ namespace http{
     {
         this->ip_address = ip_address;
         this->port = port;
+        this->socketAddress.sin_addr.s_addr =  inet_addr(ip_address.c_str());
+        this->socketAddress.sin_family = AF_INET;
+        this->socketAddress.sin_port = port;
         startServer();
     }
 
@@ -34,6 +44,15 @@ namespace http{
         return 0; 
     }
 
+    bool TcpServer::acceptConnection(int &newSocket){
+        newSocket = accept(serverSocket, (sockaddr *)&socketAddress, &socketAddress_length);
+        if(newSocket < 0){
+            // throw error
+            return false;
+        }
+        return true;
+    }
+
     bool TcpServer::closeServer(){
         close(serverSocket);
         close(newSocket);
@@ -42,13 +61,15 @@ namespace http{
     }
 
     bool TcpServer::startListen(){
+        std::cout << "TRIAL\n";
         int listenSuccess = listen(serverSocket, 20);
         if(listenSuccess < 0){
             // throw an error
             return false;
         }
-
+        
         // log the server has startted listening stuff around here.
+        int bytesReceived;
         
         
 
